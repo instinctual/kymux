75fb6a17985d86e609311a94dfb3cab3a5365a60
Romain Vimont <rom@rom1v.com>
2023-04-12 11:31:25 +0200

    Use SafeUnreliableProtocol

--- kymux/src/endpoint.rs
+++ kymux/src/endpoint.rs
@@ -2,7 +2,7 @@ use log::{debug, error};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
-use crate::protocol::gopstream::GopStreamProtocol;
+use crate::protocol::safe_unreliable::SafeUnreliableProtocol;
 use crate::protocol::{Protocol, SimpleBiProtocol, SimpleUniProtocol};
 use crate::router::KyChannel;
 use crate::{Error, Result, StreamOwner, StreamType};
@@ -114,7 +114,7 @@ impl Endpoint {
 
         let mut protocol: Box<dyn Protocol + Send> = match desc.type_ {
             StreamType::Input => Box::new(SimpleBiProtocol::new(desc)),
-            StreamType::Video => Box::new(GopStreamProtocol::new(desc)),
+            StreamType::Video => Box::new(SafeUnreliableProtocol::new(desc)),
             StreamType::Audio => Box::new(SimpleUniProtocol::new(desc)),
         };
 
