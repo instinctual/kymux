8930bd93c555766e2ea92887641d036ab98851a2
Romain Vimont <rom@rom1v.com>
2023-06-05 15:56:47 +0200

    Configure video protocol via an envvar
    Set KYMUX_PROTOCOL_VIDEO to:
     - "reliable" (default)
     - "gopstream"
     - "unreliable_fec"

--- kymux/src/endpoint.rs
+++ kymux/src/endpoint.rs
@@ -1,7 +1,8 @@
-use log::{debug, error};
+use log::{debug, error, info, warn};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
+use crate::protocol::gopstream::GopStreamProtocol;
 use crate::protocol::unreliable_fec::UnreliableFecProtocol;
 use crate::protocol::{Protocol, SimpleBiProtocol, SimpleUniProtocol};
 use crate::router::KyChannel;
@@ -114,7 +115,28 @@ impl Endpoint {
 
         let mut protocol: Box<dyn Protocol + Send> = match desc.type_ {
             StreamType::Input => Box::new(SimpleBiProtocol::new(desc)),
-            StreamType::Video => Box::new(UnreliableFecProtocol::new(desc)),
+            StreamType::Video => {
+                let protocol_name =
+                    std::env::var("KYMUX_PROTOCOL_VIDEO").unwrap_or("reliable".to_string());
+                match protocol_name.as_str() {
+                    "reliable" => {
+                        info!("Using video protocol 'reliable'");
+                        Box::new(SimpleUniProtocol::new(desc))
+                    }
+                    "gopstream" => {
+                        info!("Using video protocol 'gopstream'");
+                        Box::new(GopStreamProtocol::new(desc))
+                    }
+                    "unreliable_fec" => {
+                        info!("Using video protocol 'unreliable_fec'");
+                        Box::new(UnreliableFecProtocol::new(desc))
+                    }
+                    _ => {
+                        warn!("Unsupported protocol: {protocol_name}, using 'reliable'");
+                        Box::new(SimpleUniProtocol::new(desc))
+                    }
+                }
+            }
             StreamType::Audio => Box::new(SimpleUniProtocol::new(desc)),
         };
 
