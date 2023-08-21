2c65e122c82396078d15bdcc66b07c5076f04cf5
Romain Vimont <rom@rom1v.com>
2023-08-21 16:36:44 +0200

    First API draft

---
+++ src/error.rs
@@ -0,0 +1,41 @@
+use thiserror::Error;
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+pub enum ConnectionError {
+    #[error("generic connection error")]
+    Generic,
+}
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+pub enum SendDatagramError {
+    #[error("datagram unsupported by peer")]
+    Unsupported,
+    #[error("datagram too large")]
+    TooLarge,
+    #[error("connection error")]
+    ConnectionError(#[from] ConnectionError),
+}
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+pub enum ReadError {
+    #[error("generic read error")]
+    Generic,
+}
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+pub enum ReadExactError {
+    #[error("read exact finished early")]
+    FinishedEarly,
+    #[error("read error")]
+    ReadError(#[from] ReadError),
+}
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+pub enum WriteError {
+    #[error("generic write error")]
+    Generic,
+}
+
+#[derive(Debug, Error, Clone, Eq, PartialEq)]
+#[error("unknown stream")]
+pub struct UnknownStreamError;
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,101 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use crate::error::*;
+
+use bytes::Bytes;
+use std::net::SocketAddr;
+
+mod error;
+
+pub struct Endpoint {
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Endpoint {
+    pub fn client(bind_addr: SocketAddr) -> std::io::Result<Self> {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    }
+
+    pub fn server(config: ServerConfig, bind_addr: SocketAddr) -> std::io::Result<Self> {
+
+    }
+
+    pub async fn connect(&self, addr: SocketAddr) -> Result<Connection, ConnectionError> {
+
+    }
+
+    pub async fn accept(&self) -> Option<Connection> {
+
+    }
+}
+
+pub struct ServerConfig {
+}
+
+pub struct Connection {
+}
+
+impl Connection {
+    pub async fn open_uni(&self) -> Result<SendStream, ConnectionError> {
+
+    }
+
+    pub async fn open_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError> {
+
+    }
+
+    pub async fn accept_uni(&self) -> Result<RecvStream, ConnectionError> {
+
+    }
+
+    pub async fn accept_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError> {
+
+    }
+
+    pub async fn read_datagram(&self) -> Result<Bytes, ConnectionError> {
+
+    }
+
+    pub async fn send_datagram(&self, data: Bytes) -> Result<(), SendDatagramError> {
+
+    }
+
+    pub fn close(&self, error_code: u64, reason: &str) {
+
+    }
+
+    pub fn max_datagram_size(&self) -> Option<usize> {
+
+    }
+}
+
+pub struct SendStream {
+}
+
+impl SendStream {
+    pub async fn write(&mut self, buf: &[u8]) -> Result<usize, WriteError> {
+
+    }
+
+    pub async fn write_all(&mut self, buf: &[u8]) -> Result<(), WriteError> {
+
+    }
+
+    pub async fn close(&self) -> Result<(), WriteError> {
+
+    }
+
+    pub fn abort(&self) -> Result<(), UnknownStreamError> {
+
+    }
+}
+
+pub struct RecvStream {
+}
+
+impl RecvStream {
+    pub async fn read(&mut self, buf: &mut [u8]) -> Result<Option<usize>, ReadError> {
+
+    }
+
+    pub async fn read_exact(&mut self, buf: &mut [u8]) -> Result<(), ReadExactError> {
+
     }
 }
