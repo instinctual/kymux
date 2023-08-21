8a2726719e751258cc8b12e37073e2bfee904f8a
Romain Vimont <rom@rom1v.com>
2023-08-21 17:24:31 +0200

    Add driver API

---
+++ src/driver/mod.rs
@@ -0,0 +1,42 @@
+use crate::error::*;
+use crate::{RecvStream, SendStream};
+
+use async_trait::async_trait;
+use bytes::Bytes;
+
+#[async_trait]
+pub trait ConnectionDriver {
+    async fn open_uni(&self) -> Result<SendStream, ConnectionError>;
+
+    async fn open_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError>;
+
+    async fn accept_uni(&self) -> Result<RecvStream, ConnectionError>;
+
+    async fn accept_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError>;
+
+    async fn read_datagram(&self) -> Result<Bytes, ConnectionError>;
+
+    async fn send_datagram(&self, data: Bytes) -> Result<(), SendDatagramError>;
+
+    fn close(&self, error_code: u64, reason: &str);
+
+    fn max_datagram_size(&self) -> Option<usize>;
+}
+
+#[async_trait]
+pub trait SendStreamDriver {
+    async fn write(&mut self, buf: &[u8]) -> Result<usize, WriteError>;
+
+    async fn write_all(&mut self, buf: &[u8]) -> Result<(), WriteError>;
+
+    async fn close(&self) -> Result<(), WriteError>;
+
+    fn abort(&self) -> Result<(), UnknownStreamError>;
+}
+
+#[async_trait]
+pub trait RecvStreamDriver {
+    async fn read(&mut self, buf: &mut [u8]) -> Result<Option<usize>, ReadError>;
+
+    async fn read_exact(&mut self, buf: &mut [u8]) -> Result<(), ReadExactError>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,92 @@
+use crate::driver::{ConnectionDriver, RecvStreamDriver, SendStreamDriver};
 use crate::error::*;
 
 use bytes::Bytes;
-use std::net::SocketAddr;
 
+mod driver;
 mod error;
 
 // This abtraction does not handle the QUIC or WebTransport connection
 // creation, but only streams and datagram once the connection is created
 
 pub struct Connection {
+    driver: Box<dyn ConnectionDriver>,
 }
 
 impl Connection {
     pub async fn open_uni(&self) -> Result<SendStream, ConnectionError> {
-
+        self.driver.open_uni().await
     }
 
     pub async fn open_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError> {
-
+        self.driver.open_bi().await
     }
 
     pub async fn accept_uni(&self) -> Result<RecvStream, ConnectionError> {
-
+        self.driver.accept_uni().await
     }
 
     pub async fn accept_bi(&self) -> Result<(SendStream, RecvStream), ConnectionError> {
-
+        self.driver.accept_bi().await
     }
 
     pub async fn read_datagram(&self) -> Result<Bytes, ConnectionError> {
-
+        self.driver.read_datagram().await
     }
 
     pub async fn send_datagram(&self, data: Bytes) -> Result<(), SendDatagramError> {
-
+        self.driver.send_datagram(data).await
     }
 
     pub fn close(&self, error_code: u64, reason: &str) {
-
+        self.driver.close(error_code, reason)
     }
 
     pub fn max_datagram_size(&self) -> Option<usize> {
-
+        self.driver.max_datagram_size()
     }
 }
 
 pub struct SendStream {
+    driver: Box<dyn SendStreamDriver>,
 }
 
 impl SendStream {
-    pub async fn write(&mut self, buf: &[u8]) -> Result<usize, WriteError> {
+    pub(crate) fn new(driver: Box<dyn SendStreamDriver>) -> Self {
+        Self { driver }
+    }
 
+    pub async fn write(&mut self, buf: &[u8]) -> Result<usize, WriteError> {
+        self.driver.write(buf).await
     }
 
     pub async fn write_all(&mut self, buf: &[u8]) -> Result<(), WriteError> {
-
+        self.driver.write_all(buf).await
     }
 
     pub async fn close(&self) -> Result<(), WriteError> {
-
+        self.driver.close().await
     }
 
     pub fn abort(&self) -> Result<(), UnknownStreamError> {
-
+        self.driver.abort()
     }
 }
 
 pub struct RecvStream {
+    driver: Box<dyn RecvStreamDriver>,
 }
 
 impl RecvStream {
-    pub async fn read(&mut self, buf: &mut [u8]) -> Result<Option<usize>, ReadError> {
+    pub(crate) fn new(driver: Box<dyn RecvStreamDriver>) -> Self {
+        Self { driver }
+    }
 
+    pub async fn read(&mut self, buf: &mut [u8]) -> Result<Option<usize>, ReadError> {
+        self.driver.read(buf).await
     }
 
     pub async fn read_exact(&mut self, buf: &mut [u8]) -> Result<(), ReadExactError> {
-
+        self.driver.read_exact(buf).await
     }
 }
