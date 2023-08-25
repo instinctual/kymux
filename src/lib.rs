f8cab4580a0c79997224458af2476e5a4de7d47c
Romain Vimont <rom@rom1v.com>
2023-08-25 16:04:29 +0200

    Make error types public

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use bytes::Bytes;
 
 mod driver;
-mod error;
+pub mod error;
 
 // This abtraction does not handle the QUIC or WebTransport connection
 // creation, but only streams and datagram once the connection is created
