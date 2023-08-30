760556f5f344239bf6989bfb1cbc2a027dc685a1
Romain Vimont <rom@rom1v.com>
2023-08-30 16:25:10 +0200

    Add spawn abstraction

---
+++ kyproto/src/runtime.rs
@@ -0,0 +1,21 @@
+use std::future::Future;
+
+#[cfg(all(feature = "tokio", not(target_family = "wasm")))]
+pub(crate) fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + Send + 'static,
+{
+    tokio::spawn(future);
+}
+#[cfg(all(feature = "tokio", target_family = "wasm"))]
+compile_error!("Tokio runtime is not available for wasm");
+
+#[cfg(all(feature = "js", target_family = "wasm"))]
+pub(crate) fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+#[cfg(all(feature = "js", not(target_family = "wasm")))]
+compile_error!("JS runtime is only available for wasm");
