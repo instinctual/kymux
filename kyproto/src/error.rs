bfd4f5ebfdc7e660689b7cf5a8a0dbecac32e052
Romain Vimont <rom@rom1v.com>
2023-09-14 16:15:23 +0200

    Provide conversion from errors to JsValue
    For convenience.

--- kyproto/src/error.rs
+++ kyproto/src/error.rs
@@ -13,3 +13,30 @@ pub struct ProtocolError(pub String);
 #[derive(Debug, Error, Clone)]
 #[error("protocol start error")]
 pub struct ProtocolStartError;
+
+#[cfg(all(feature = "js", target_family = "wasm"))]
+mod js_error {
+    // For convenience, also provide the conversion from these errors to
+    // JsValue (containing the error message). They could not yield the
+    // original JsValue, which is not stored in the kyproto errors).
+    //
+    // This implementation could not be done by the client, since neither
+    // JsValue nor kyproto errors are defined in the client crate.
+
+    macro_rules! impl_from_jsvalue {
+        ($t:ty) => {
+            impl From<$t> for wasm_bindgen::JsValue {
+                fn from(value: $t) -> Self {
+                    Self::from(&value.to_string())
+                }
+            }
+        };
+    }
+
+    impl_from_jsvalue!(crate::error::EndpointAlreadyRegistered);
+    impl_from_jsvalue!(crate::error::ProtocolError);
+    impl_from_jsvalue!(crate::error::ProtocolStartError);
+}
+
+#[cfg(all(feature = "js", target_family = "wasm"))]
+pub use js_error::*;
