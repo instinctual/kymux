1794d02a1e48d6c67e75d0814ebcec5bd48a2199
Romain Vimont <rom@rom1v.com>
2023-09-19 18:05:23 +0200

    Remove NonWasmSend trait
    It was not as useful as expected, since it is not an auto-trait. And it
    caused very confusing error messages on unrelated errors.

--- kyproto/src/task.rs
+++ kyproto/src/task.rs
@@ -11,26 +11,41 @@ pub(crate) struct Task {
     tx: oneshot::Sender<()>,
 }
 
-impl Task {
-    pub(crate) fn spawn_task<F>(task: F, name: String) -> Self
-    where
-        F: Future<Output = ()> + runtime::NonWasmSend + 'static,
-    {
+macro_rules! impl_spawn_task {
+    ($task:expr, $name:expr) => {{
         let (tx, rx) = oneshot::channel();
-        let task_name = name.clone();
+        let task_name = $name.clone();
 
         runtime::spawn(async move {
             tokio::select! {
                 _ = rx => {
                     debug!("Task {task_name} interrupted");
                 }
-                _ = task => {
+                _ = $task => {
                     debug!("Task {task_name} terminated");
                 }
             }
         });
 
-        Task { name, tx }
+        Task { name: $name, tx }
+    }};
+}
+
+impl Task {
+    #[cfg(all(feature = "tokio-rt", not(target_family = "wasm")))]
+    pub(crate) fn spawn_task<F>(task: F, name: String) -> Self
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        impl_spawn_task!(task, name)
+    }
+
+    #[cfg(all(feature = "js", target_family = "wasm"))]
+    pub(crate) fn spawn_task<F>(task: F, name: String) -> Self
+    where
+        F: Future<Output = ()> + 'static,
+    {
+        impl_spawn_task!(task, name)
     }
 
     pub(crate) fn cancel(self) -> Result<(), ()> {
