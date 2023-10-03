8c33f567faff45dae3147648aeef4e347411f4e6
Romain Vimont <rom@rom1v.com>
2023-10-03 18:42:41 +0200

    Make ProtocolSend/ProtocolRecv Send on non-wasm
    Otherwise, it is not possible to hold a protocol instance across awaits
    with tokio.

--- kyproto/src/protocol/driver/mod.rs
+++ kyproto/src/protocol/driver/mod.rs
@@ -9,17 +9,33 @@ pub(crate) mod reliable;
 pub(crate) mod unreliable_fec;
 pub(crate) mod util;
 
+#[cfg(target_family = "wasm")]
 #[cfg_attr(target_family = "wasm", async_trait(?Send))]
-#[cfg_attr(not(target_family = "wasm"), async_trait)]
 pub(crate) trait ProtocolSendDriver {
     type Packet;
 
     async fn send(&mut self, packet: Self::Packet) -> Result<(), ProtocolError>;
 }
 
-#[cfg_attr(target_family = "wasm", async_trait(?Send))]
+#[cfg(not(target_family = "wasm"))]
 #[cfg_attr(not(target_family = "wasm"), async_trait)]
+pub(crate) trait ProtocolSendDriver: Send {
+    type Packet;
+
+    async fn send(&mut self, packet: Self::Packet) -> Result<(), ProtocolError>;
+}
+
+#[cfg(target_family = "wasm")]
+#[cfg_attr(target_family = "wasm", async_trait(?Send))]
 pub(crate) trait ProtocolRecvDriver {
+    type Packet;
+
+    async fn recv(&mut self) -> Result<Option<Self::Packet>, ProtocolError>;
+}
+
+#[cfg(not(target_family = "wasm"))]
+#[cfg_attr(not(target_family = "wasm"), async_trait)]
+pub(crate) trait ProtocolRecvDriver: Send {
     type Packet;
 
     async fn recv(&mut self) -> Result<Option<Self::Packet>, ProtocolError>;
