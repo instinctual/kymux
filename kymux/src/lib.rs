0709ff4898c8fcada10c97fcd38b06cf41de5258
Romain Roffé <romain@kyber.media>
2024-03-29 13:46:20 +0100

    Revert "kymux: Configure max_idle_timeout"
    This reverts commit 5f8dc8766ad96955e83c386ecaf0a42648e0af72.
    
    (cherry picked from commit 2dfdaa5c5cc6a0872f49a8e0a8cd2711b2575bfe)

--- kymux/src/lib.rs
+++ kymux/src/lib.rs
@@ -16,9 +16,6 @@ pub use kyproto::{
 const KYMUX_LOCAL_CLIENTS_PORT: u16 = 9090;
 
 #[allow(dead_code)]
-const MAX_IDLE_TIMEOUT: Duration = Duration::from_secs(20);
-
-#[allow(dead_code)]
 const KEEP_ALIVE_INTERVAL: Duration = Duration::from_secs(5);
 
 pub enum ServerConfig {
@@ -82,7 +79,7 @@ impl Server {
                     private_key.into(),
                     &kyproto::quinn::QuinnServerOptions {
                         keep_alive_interval: Some(KEEP_ALIVE_INTERVAL),
-                        max_idle_timeout: Some(MAX_IDLE_TIMEOUT),
+                        ..Default::default()
                     },
                 )?;
 
@@ -100,7 +97,7 @@ impl Server {
                     kyproto::cert::PrivateKey::new(private_key),
                     &kyproto::wtransport::WTransportServerOptions {
                         keep_alive_interval: Some(KEEP_ALIVE_INTERVAL),
-                        max_idle_timeout: Some(MAX_IDLE_TIMEOUT),
+                        ..Default::default()
                     },
                 )?;
 
@@ -204,7 +201,7 @@ impl Connection {
                     roots.into(),
                     &kyproto::quinn::QuinnClientOptions {
                         keep_alive_interval: Some(KEEP_ALIVE_INTERVAL),
-                        max_idle_timeout: Some(MAX_IDLE_TIMEOUT),
+                        ..Default::default()
                     },
                 )
                 .await?
