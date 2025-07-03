e81d71e23870a50c42b06b2d77bbf6c3914d1c33
Romain Vimont <rom@rom1v.com>
2025-07-03 09:55:42 +0200

    kycom: Add client API
    Add an API to send and receive packets to an IPC endpoint.

--- kycom/src/lib.rs
+++ kycom/src/lib.rs
@@ -3,10 +3,15 @@ use std::net::SocketAddr;
 #[allow(unused)]
 use log::{debug, error, info, warn};
 
+mod client;
 mod ipc;
 mod serial;
 mod server;
 
+pub use client::{
+    InputEndpoint, IpcEndpoint, MetricsClientEndpoint, MetricsServerEndpoint, VideoClientEndpoint,
+    VideoServerEndpoint,
+};
 pub use ipc::{Ipc, IpcRecv, IpcSend};
 pub use server::{Forwarder, KyCom, TcpForwarder};
 
